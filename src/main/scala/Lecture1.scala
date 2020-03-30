3243c50bbad13f21c0c9f8afd85c25694435b3d7
Angel <adcalder7@gmail.com>
2020-03-29 23:21:31 -0400

    Finishing lesson

--- src/main/scala/Lecture1.scala
+++ src/main/scala/Lecture1.scala
@@ -1,3 +1,5 @@
+// https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-006-introduction-to-algorithms-fall-2011/lecture-videos/lecture-1-algorithmic-thinking-peak-finding/
+
 // Divide and Conquer
 object Lecture1 {
 
